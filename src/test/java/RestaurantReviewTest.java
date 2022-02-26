1491099eada51be9c0e0ed3978972a062900da91
Anil Menon <99anilmenon@gmail.com>
2022-02-26 13:08:22 -0500

    Update RestaurantReviewTest.java
    Added Assert import

--- src/test/java/RestaurantReviewTest.java
+++ src/test/java/RestaurantReviewTest.java
@@ -2,6 +2,8 @@
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.Assert.assertEquals;
+
 @SpringBootTest
 public class RestaurantReviewTest {
 
