b98b8485373c2786a36eafaa9e7e09c207f77f1d
Anil Menon <99anilmenon@gmail.com>
2022-02-26 14:32:05 -0500

    Update Restaurant.java
    Added restaurant annotations and included restaurant review methods

--- src/main/java/com/trg/restaurantreview/Restaurant.java
+++ src/main/java/com/trg/restaurantreview/Restaurant.java
@@ -1,10 +1,30 @@
 package com.trg.restaurantreview;
+import javax.persistence.*;
+import java.util.*;
 
+@Entity
 public class Restaurant {
+    @Id
+    @GeneratedValue
+    private Integer id;
     private String name;
     private String address;
     private String phoneNumber;
     private String description;
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private List<RestaurantReview> restaurant;
+
+    public Restaurant() {
+        this.restaurant = new ArrayList<>();
+    } //javabeans convention
+
+    public Restaurant(String name, String address, String phoneNumber, String description){
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.description = description;
+        this.restaurant = new ArrayList<>();
+    }
 
     public String getName() {
         return name;
@@ -37,4 +57,17 @@ public class Restaurant {
     public void setDescription(String description) {
         this.description = description;
     }
+
+    public void addReview(RestaurantReview review) {
+        this.restaurant.add(review);
+    }
+
+    public void removeReview(RestaurantReview review) {
+       this.restaurant.remove(review);
+    }
+
+    public List<RestaurantReview> getReviews() {
+        return this.restaurant;
+    }
+
 }
