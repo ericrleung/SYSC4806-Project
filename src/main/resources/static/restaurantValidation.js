e5068a56b1ef0b9429fc0f5138da39d76cbde745
ericrleung <eric.leung.iii@gmail.com>
2022-03-12 11:29:25 -0500

    Add input validation

--- src/main/resources/static/restaurantValidation.js
+++ src/main/resources/static/restaurantValidation.js
@@ -2,12 +2,26 @@ var addressField = document.getElementById("address");
 var phoneField = document.getElementById("phoneNum");
 
 function validateForm() {
-  if (addressField.value == "") {
-    alert("Address Should have format \"{number} {street name} Optional:{City/Country/Postal Code}\"");
-    return false;
+  let addressPattern = new RegExp("[0-9]+\\s.+");
+  let phonePattern = new RegExp("^(\\+\\d{1,2}\\s)?\\(?\\d{3}\\)?[\\s.-]?\\d{3}[\\s.-]?\\d{4}$");
+  console.log(!addressPattern.test(addressField.value))
+  console.log(!phonePattern.test(phoneField.value))
+  let text = "";
+  var valid = new Boolean(true);
+  if (!addressPattern.test(addressField.value)) {
+    text = text.concat("Address Should have format \"{number} {street name} Optional:{City/Country/Postal Code}\"");
+    valid = false;
   }
-  else if(phoneField.value == ""){
-    alert("Phone Number Should follow format \"(XXX)-(XXX)-(XXXX)\"");
-    return false;
+  if(!phonePattern.test(phoneField.value)){
+    if (text.length) {
+      text = text.concat("\n\nPhone Number Should follow format \"XXX-XXX-XXXX\"");
+    } else {
+     text = text.concat("Phone Number Should follow format \"XXX-XXX-XXXX\"");
+    }
+    valid = false;
   }
+  if (!valid) {
+    alert(text);
+  }
+  return valid;
 }
