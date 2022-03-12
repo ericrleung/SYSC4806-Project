ce09fb873e82e083fabdb8cc2e913102334b9c1e
dylan <dylan.leveille321@gmail.com>
2022-03-12 11:44:07 -0500

    Finish Input Validation for Review

---
+++ src/main/resources/static/reviewValidation.js
@@ -0,0 +1,12 @@
+var rating = document.getElementById("rating");
+
+function validateForm() {
+  let ratingPattern = new RegExp("[1-5]");
+  var valid = new Boolean(true);
+  if (!ratingPattern.test(rating.value)) {
+    alert("Rating should be number in range [1,5]")
+    valid = false;
+  }
+
+  return valid;
+}
