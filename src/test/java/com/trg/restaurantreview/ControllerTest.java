876c0460191978943a58c9e3d62bd7d65820c821
KhalilAalab <khalilaalab@cmail.carleton.ca>
2022-03-19 11:28:04 -0400

    Added delete review test case

--- src/test/java/com/trg/restaurantreview/ControllerTest.java
+++ src/test/java/com/trg/restaurantreview/ControllerTest.java
@@ -107,8 +107,23 @@ public class ControllerTest {
     }
 
     @Test
-    public void removeRestaurantReview(){
-
+    public void removeRestaurantReview() throws Exception{
+        this.mvc.perform((post("/createrestaurant")
+                        .param("name", "sampleRestaurant")
+                        .param("address", "1125 ColonelBy Drive")
+                        .param("phoneNumber", "613-123-4567")))
+                .andDo(print());
+        this.mvc.perform(post("/addingReview")
+                        .param("restaurantID", String.valueOf(7))
+                        .param("rating", String.valueOf(5))
+                        .param("message", "hello")
+                        .param("reviewerName", "Yathu"))
+                .andDo(print());
+        this.mvc.perform(post("/deletereview")
+                        .param("restaurantid", "7")
+                        .param("reviewid", "5"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("sampleRestaurant")));
 
     }
 
